60a4ae3d2048480c676745985b2e354c25562cd1
“zenickn” <nicksonn95@gmail.com>
2020-08-22 21:01:01 +0800

    project init

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard()
+  ));
+}
+
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title:Text('NinjaIDCard'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,//drop shadow
+
+      ),
+      body:Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpeg'),
+                radius: 40.0,
+              ),
+            ),
+           Divider(
+             height:90,
+             color:Colors.grey[800],
+           ),
+           Text('Name',
+           style: TextStyle(
+             color:Colors.grey,
+             letterSpacing: 2,
+           ),
+           ),
+            SizedBox(height:10),//add space
+            Text('Chun-Li',
+              style: TextStyle(
+                color:Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height:30),//add space
+            Text('Current Ninja Level',
+              style: TextStyle(
+                color:Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height:10),//add space
+            Text('8',
+              style: TextStyle(
+                  color:Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(height:30),//add space
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+
+                ),
+                SizedBox(width:10),//add space
+                Text(
+                  'testing@gmail.com',
+                  style:TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing:1
+
+                  )
+                ),
+              ],
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
+
+
+
+
